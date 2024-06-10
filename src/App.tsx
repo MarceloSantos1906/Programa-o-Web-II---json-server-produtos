617c8555c7cdf60b3d89f8e1538f2fd15035c013
MarceloSantos1906 <marcelo_santos1906@hotmail.com>
2024-06-10 20:35:13 -0300

    Added SweetAlerts2

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,11 @@ import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { useEffect, useState } from 'react';
 import Table from './Table/Table';
+import Swal from 'sweetalert2';
 import api from './api';
 import './App.css';
 
-function App() {
+function List() {
   const [product, setProduct] = useState({
     name: '',
     price: '',
@@ -21,105 +22,171 @@ function App() {
     setProduct(newProduct);
   };
 
-  async function getProduct() {
-    await api.get('/products').then((response) => setProducts(response.data));
-  }
-
-  async function onEdit(id) {
-    await api.get(`/produtos/${id}`).then((res) => setProduto(res.data));
-  }
-
-  function onDelete(id) {
-    if (confirm('Deseja excluir ${name} ?')) {
-      api
-        .delete(`/produtos/${id}`)
-        .then((result) => {
-          toast.done('Excluído com sucesso.');
-          getProdutos();
-        })
-        .catch((e) => {
-          toast.error('Não foi possivel excluir ${name}');
-        });
-    }
-  }
-
-  useEffect(() => {
-    getProduct();
-  }, []);
-
-  const register = () => {
+  const cadastrar = () => {
     if (product.name == '') {
       toast.error('Nome  não pode ser vazio');
       return;
     } else if (Number(product.price) <= 0) {
-      toast.error('Valor não pode ser negativo');
+      toast.error('Salario não pode ser negativo');
       return;
     } else if (product.price == '') {
-      toast.error('Valor não pode ser vazio');
+      toast.error('Salario não pode ser vazio');
       return;
     }
 
     if (product.id) {
       api
-        .put('/products/${product.id}', product)
+        .patch(`products/${product.id}`, product)
         .then((response) => {
-          toast.success('Alterado com sucesso');
+          Swal.fire({
+            position: 'top-end',
+            icon: 'success',
+            title: 'Alterado com sucesso',
+            showConfirmButton: false,
+            timer: 10000,
+          });
           setProduct({ name: '', price: '', description: '' });
-          getProduct();
+          getProducts();
         })
         .catch((e) => {
-          toast.error('Erro ao efetuar cadastro');
+          Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Erro ao efetuar o cadastro: ' + e,
+          });
         });
     } else {
       api
-        .post('/products', product)
+        .post('products', product)
         .then((response) => {
-          toast.success('Cadastrado com sucesso');
+          Swal.fire({
+            position: 'top-end',
+            icon: 'success',
+            title: 'Cadastro efetuado com sucesso',
+            showConfirmButton: false,
+            timer: 10000,
+          });
           setProduct({ name: '', price: '', description: '' });
-          getProduct();
+          getProducts();
         })
         .catch((e) => {
-          toast.error('Erro ao efetuar cadastro');
+          Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Erro ao efetuar o cadastro: ' + e,
+          });
         });
     }
   };
 
+  async function getProducts() {
+    await api.get('products').then((response) => setProducts(response.data));
+  }
+
+  async function onEdit(id) {
+    await api
+      .get(`products/${id}`)
+      .then((response) => setProduct(response.data));
+  }
+
+  function onDelete(id) {
+    const swalWithBootstrapButtons = Swal.mixin({
+      customClass: {
+        confirmButton: 'btn btn-success',
+        cancelButton: 'btn btn-danger',
+      },
+      buttonsStyling: false,
+    });
+    swalWithBootstrapButtons
+      .fire({
+        title: 'Tem certeza que deseja excluir?',
+        text: 'Não será possivel reverter!',
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonText: 'Sim, deletar!',
+        cancelButtonText: 'Não, cancelar!',
+        reverseButtons: true,
+      })
+      .then((result) => {
+        if (result.isConfirmed) {
+          api
+            .delete(`products/${id}`)
+            .then((response) => {
+              toast.done('Excluído com sucesso.');
+              getProducts();
+            })
+            .catch((e) => {
+              if (e == 'AxiosError: Request failed with status code 404') {
+                Swal.fire({
+                  icon: 'error',
+                  title: 'Oops...',
+                  text: 'Cadastro não encontrado',
+                });
+              } else {
+                toast.error(e);
+              }
+            });
+          swalWithBootstrapButtons.fire({
+            title: 'Deletado!',
+            text: 'Funcionario foi deletado.',
+            icon: 'success',
+          });
+        } else if (result.dismiss === Swal.DismissReason.cancel) {
+          swalWithBootstrapButtons.fire({
+            title: 'Cancelado',
+            icon: 'error',
+          });
+        }
+      });
+  }
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+
   return (
     <>
-      <div>
-        <h2>Cadastro de Produtos</h2>
-        <div>
-          <label>Nome: </label>
+      <div className="container">
+        <h2>Cadastro de products</h2>
+        <div className="mb-3">
+          <label class="form-label">Nome: </label>
           <input
             type="text"
             name="name"
-            value={product.name}
+            defaultValue={product.name}
             onChange={handleChange}
-            defaultValue="tset"
+            className="form-control"
           />
         </div>
-        <div>
-          <label>Preço: </label>
+        <div className="mb-3">
+          <label class="form-label">Preço: </label>
           <input
             type="number"
+            step="0.01"
+            min="0"
             name="price"
-            value={product.price}
+            defaultValue={product.price}
             onChange={handleChange}
-            defaultValue="tset"
+            className="form-control"
           />
         </div>
-        <div>
-          <label>Descição: </label>
+        <div className="mb-3">
+          <label class="form-label">Descrição: </label>
           <input
             type="text"
             name="description"
-            value={product.description}
+            defaultValue={product.description}
             onChange={handleChange}
-            defaultValue="tset"
+            className="form-control"
           />
         </div>
-        <div>
-          <button type="button" onClick={register}>
+        <div className="col-auto">
+          <button
+            type="button"
+            key="button"
+            className="btn btn-success"
+            onClick={cadastrar}
+          >
             Cadastrar
           </button>
         </div>
@@ -130,4 +197,4 @@ function App() {
   );
 }
 
-export default App;
+export default List;
